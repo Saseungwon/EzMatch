8051b8ee543ec6d86d1bdea7b7b2cc64784e150e
dfg1425@naver.com <dfg1425@naver.com>
2022-10-20 21:55:15 +0900

    error fix

--- EzMatch-backend/src/main.js
+++ EzMatch-backend/src/main.js
@@ -3,13 +3,14 @@ import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
+
 import api from './api';
 
-// 비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
+// 비구조화 할당을 통하여 process.env 내부 값에 대한 레퍼런스 만들기
 const { PORT, MONGO_URI } = process.env;
 
 mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
+  .connect(MONGO_URI, { useNewUrlParser: true })
   .then(() => {
     console.log('Connected to MongoDB');
   })
@@ -20,39 +21,20 @@ mongoose
 const app = new Koa();
 const router = new Router();
 
-// api 라우터 설정
+// 라우터 설정
 router.use('/api', api.routes()); // api 라우트 적용
 
-// koa 라우터 설정
-router.get('/', (ctx) => {
-  ctx.body = '홈';
-});
-
-router.get('/about/:name?', (ctx) => {
-  const { name } = ctx.params;
-  // name의 존재 유무에 따라 다른 결과 출력
-  // http://localhost:4000/about/react
-  ctx.body = name ? `${name}의 소개` : '소개';
-});
-
-router.get('/posts', (ctx) => {
-  const { id } = ctx.query;
-  // id의 존재 유무에 따라 다른 결과 출력
-  // http://localhost:4000/posts?id=10
-  ctx.body = id ? `포스트 #${id}` : '포스트 아이디가 없습니다.';
-});
-
-// 라우터 적용 전에 bodyparser 적용
+// 라우터 적용 전에 bodyParser 적용
 app.use(bodyParser());
 
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
+// PORT 가 지정되어있지 않다면 4000 을 사용
 const port = PORT || 4000;
 app.listen(port, () => {
   console.log('Listening to port %d', port);
 });
-
 //
 //
 // koa 미들웨어 기본사용법
